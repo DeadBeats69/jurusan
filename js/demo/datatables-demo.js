700be500ec2a1873e474a262dbf065313eed9b32
DeadBeats69 <novalikayudistira@.com>
2022-10-27 12:11:44 +0700

    initial commit

---
+++ js/demo/datatables-demo.js
@@ -0,0 +1,10 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function () {
+  $("#dataTableKK").DataTable();
+});
+$(document).ready(function () {
+  $("#dataTableBK").DataTable();
+});
+$(document).ready(function () {
+  $("#dataTablePK").DataTable();
+});
